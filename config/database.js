57e567cefb2d7a4bae83c6dfbfab83bc467b98c6
mhtbhavsar <mohit.bhavsar@coderize.in>
2017-02-17 23:24:09 +0530

    Node js authentication with passport and session
    NodeJs authentication using passportjs and session.

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': ''
+    },
+	'database': 'chad',
+    'users_table': 'ilance_users'
+};
